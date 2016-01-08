5b0f401a869619694ece28bdf95927120393b9bc
Nguyen Phan <nqphan212@gmail.com>
2016-01-08 14:29:49 -0500

    Create a node class and set up the node

---
+++ Assets/__Scripts/Node.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Node : MonoBehaviour {
+
+	public List<Node> adjacent = new List<Node>();      //List of the node
+	public Node previous = null;						//The previous node
+	public string label = ""; 							//names for each node
+
+	public void Clear()
+	{
+		previous = null;
+	}
+}
